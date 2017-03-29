cf577e46056ce852b53a56152fa9b6ac6a535eaa
mo <metemaad@gmail.com>
2017-03-29 02:05:16 +0000

    Initial commit

---
+++ src/FPTree.java
@@ -0,0 +1,66 @@
+import java.util.Vector;
+
+/**
+ * This class store a node of FPTree
+ * Created by mohammad on 3/29/17.
+ */
+public class FPTree implements Comparable<FPTree>{
+
+    /**
+     * If this node is the root or not
+     */
+    boolean IsRoot=false;
+    /**
+     * child of this node
+     */
+    Vector<FPTree> child= new Vector<>();
+    /**
+     * parent of this node
+     */
+    FPTree parent;
+    /**
+     * this node item
+     */
+    String item;
+
+    /**
+     *
+     * @return returns the item value
+     */
+    public String getItem() {
+        return item;
+    }
+
+    /**
+     * set the item value
+     * @param item
+     */
+    public void setItem(String item) {
+        this.item = item;
+    }
+
+    /**
+     * stores the number of that this item occures
+     */
+    int cardinality;
+    /**
+     * shows a threat to the other similar items
+     */
+    FPTree thread=null;
+
+    /**
+     * compare fpTree with this node based on their cardinality
+     * @param fpTree
+     * @return returns 1 if this.cardinality> input. cardinality
+     */
+    @Override
+    public int compareTo(FPTree fpTree) {
+        if (this.cardinality>fpTree.cardinality)
+        {
+            return 1;
+        }
+        else
+            return this.cardinality < fpTree.cardinality ? -1 : 0;
+    }
+}
+
---
+++ src/main.java
@@ -0,0 +1,7 @@
+/**
+ * Created by mohammad on 3/29/17.
+ */
+public class main {
+    public static void main(String[] args) {
+    }
+    }
